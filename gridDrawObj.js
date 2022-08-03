42fc5ed5b938e7f84e0ff9e86b90dac72a43d8cc
CheesymonTony <carter_boyce@me.com>
2022-08-02 20:57:18 -0500

    first commit

---
+++ gridDrawObj.js
@@ -0,0 +1,267 @@
+class GridDraw {
+    constructor(resolution, origin, w, h, Ox, Oy){
+        this.resolution = resolution;
+        this.origin = origin;
+        this.width = w;
+        this.height = h;
+        this.lineW = 2;
+        this.originX = Ox;
+        this.originY = Oy;
+        this.gridOriginX = Ox;
+        this.gridOriginY = Oy;
+        this.gridOpacity = 100;
+        this.tickLen = 3;
+        this.spacingX = this.width/this.resolution;
+        this.spacingY = this.height/this.resolution;
+       
+        
+        
+
+
+        //Grid Point Details\\
+        this.pointText = false;
+        this.textSize = 5;
+        this.pointSize = map(this.resolution, 0, 100, .2, 3);
+
+        //Define the edges of the grid Canvas\\
+        this.rightEdge = this.originX + this.width/2;
+        this.leftEdge = this.originX - this.width/2;
+        this.topEdge = this.originY - this.height/2;
+        this.botEdge = this.originY + this.height/2;
+
+
+    }
+
+    setOrigin(x, y){
+
+        if (y < this.topEdge){
+            this.gridOriginY = this.topEdge;
+        } else if (y > this.botEdge){
+            this.gridOriginY = this.botEdge;
+        }else {
+        this.gridOriginY = y;
+        }
+        if (x < this.leftEdge){
+            this.gridOriginX = this.leftEdge;
+        } else if (x > this.rightEdge){
+            this.gridOriginX = this.rightEdge;
+        } else {
+            this.gridOriginX = x;
+        }
+        
+    }
+
+
+    setText(txt, txtSize){
+        this.pointText = txt;
+        this.textSize = txtSize;
+    }
+
+    setResolution(res){
+        this.resolution = res;
+        this.spacingX = this.width/this.resolution;
+        this.spacingY = this.height/this.resolution;
+        this.pointSize = map(this.resolution, 1, 100, 4, .6);
+
+
+    }
+
+    setGridOpacity(opacity){
+        this.gridOpacity = 2.55 * opacity;
+    }
+
+    setLineWidth(lineW){
+        this.lineW = lineW;
+    }
+
+    setPoint(x, y, r,g,b,a){
+        let newX = x * this.spacingX;
+        let newY = y * this.spacingY;
+        push();
+        translate(this.gridOriginX, this.gridOriginY);
+        noStroke();
+        fill(r,g,b,a);
+        ellipse(newX, -newY, this.pointSize);
+        if (this.pointText){
+            let textX = round(x, 3);
+            let textY = round(y, 3);
+            textSize(this.textSize);
+            text(textX + ', ' + textY, newX+5, -newY);
+        }
+        pop();
+        
+    }
+
+
+
+    show(){
+        // console.log(this.tester)
+
+        push();
+        fill(255);
+        noStroke();
+        rectMode(CENTER);
+        rect(this.originX, this.originY, this.width, this.height);
+        pop();
+
+
+        
+
+        stroke(0,0,255);
+        strokeWeight(2);
+        line(this.gridOriginX, this.gridOriginY, this.gridOriginX, this.topEdge);
+        line(this.gridOriginX, this.gridOriginY, this.gridOriginX, this.botEdge);
+
+        stroke(255,0,0);
+        strokeWeight(2);
+        line(this.gridOriginX, this.gridOriginY, this.rightEdge, this.gridOriginY);
+        line(this.leftEdge, this.gridOriginY, this.gridOriginX, this.gridOriginY);
+
+        let rightGridLines = floor((this.rightEdge - this.gridOriginX) / this.spacingX);
+        let leftGridLines = floor((this.gridOriginX - this.leftEdge) / this.spacingX);
+        let topGridLines = floor((this.gridOriginY - this.topEdge) / this.spacingY);
+        let botGridLines = floor((this.botEdge - this.gridOriginY) / this.spacingY);
+
+        for (let i = 1; i <= rightGridLines; i++){
+            let xSpace = i * this.spacingX;
+            strokeWeight(1);
+            stroke(0,0,0,this.gridOpacity);
+            line(this.gridOriginX + xSpace, this.topEdge, this.gridOriginX + xSpace, this.botEdge);
+        }
+        
+        for (let i = 1; i <= leftGridLines; i++){
+            let xSpace = i * this.spacingX;
+            strokeWeight(1);
+            stroke(0,0,0,this.gridOpacity);
+            line(this.gridOriginX - xSpace, this.topEdge, this.gridOriginX - xSpace, this.botEdge);
+        }
+
+        for (let i = 1; i <= topGridLines; i++){
+            let ySpace = i * this.spacingY;
+            strokeWeight(1);
+            stroke(0,0,0,this.gridOpacity);
+            line(this.leftEdge, this.gridOriginY - ySpace, this.rightEdge, this.gridOriginY - ySpace);
+        }
+
+        for (let i = 1; i <= botGridLines; i++){
+            let ySpace = i * this.spacingY;
+            strokeWeight(1);
+            stroke(0,0,0,this.gridOpacity);
+            line(this.leftEdge, this.gridOriginY + ySpace, this.rightEdge, this.gridOriginY + ySpace);
+        }
+
+
+
+
+
+        // for (let i = 0; i <= this.resolution/2; i++){
+        //     let x = this.spacingX*i;
+        //     let y = this.spacingY*i;
+        //     //layout X gridlines\\
+            
+            
+            
+        //     stroke(0,0,0,this.gridOpacity);
+        //     if (i == 0){
+        //         push();
+        //         stroke(255,0,0, 255);
+        //         line(this.originX + x, this.originY - this.height/2, this.originX + x, this.originY + this.height/2);
+        //         stroke(0,0,255, 255);
+        //         line(this.originX - this.width/2, this.originY + y , this.originX + this.width/2, this.originY + y);
+        //         pop();
+        //     } else {
+        //         line(this.originX + x, this.originY - this.height/2, this.originX + x, this.originY + this.height/2);
+                
+        //         line(this.originX- x, this.originY - this.height/2, this.originX - x, this.originY + this.height/2);
+        //         line(this.originX - this.width/2, this.originY + y , this.originX + this.width/2, this.originY + y);
+        //         line(this.originX - this.width/2, this.originY - y , this.originX + this.width/2, this.originY - y);
+               
+        //         //layout tick marks\\
+        //         push();
+        //         stroke(0,0,0,255);
+        //         line(this.originX + x, this.originY - this.tickLen, this.originX + x, this.originY + this.tickLen);
+        //         line(this.originX - x, this.originY - this.tickLen, this.originX - x, this.originY + this.tickLen);
+        //         line(this.originX - this.tickLen, this.originY + y, this.originX + this.tickLen, this.originY + y);
+        //         line(this.originX - this.tickLen, this.originY - y, this.originX + this.tickLen, this.originY - y);
+                
+        //         pop();
+        //         push();
+        //         fill(0);
+        //         noStroke();
+        //         textSize(6);
+        //         text(i, this.originX + x -5, this.originY + this.tickLen*3);
+        //         text(-i, this.originX - x, this.originY + this.tickLen*3);
+        //         text(i, this.originX - this.tickLen*3, this.originY - y+7);
+        //         text(-i, this.originX - this.tickLen*3, this.originY + y-2);
+
+        //         pop();
+
+        //     }
+            
+        //     // line(this.originX- x, this.originY - this.height/2, this.originX - x, this.originY + this.height/2);
+
+        //     // line(0, y, this.width, y);
+        //     // line(0, y, this.width, y);
+        //     // line(0, y, this.width, y);
+        // }
+
+        if (this.origin == 'CENTER'){
+            //Layout Grid\\
+            translate(this.originX, this.originY);
+            rectMode(CENTER);
+            push();
+            noStroke();
+            rect(this.width/2, this.height/2, this.width, this.height)
+            pop();
+            let spacingY = this.height / this.resolution;
+            let spacingX = this.width / this.resolution;
+            for (let i = 0; i <= this.resolution; i++){
+                let y = spacingY*i;
+                let x = spacingX*i;
+                stroke(55, 55, 55, 25);
+                push();
+                strokeWeight(this.lineW);
+                line(0, y, this.width, y);
+                line(x, 0, x, this.height);
+                line()
+                pop();
+            }
+
+            //Layout Origin Lines\\
+            push();
+            stroke(255,0,0);
+            strokeWeight(1);
+            line(0, this.height/2, this.width, this.height/2);
+            pop();
+
+            push();
+            stroke(0,0,255);
+            strokeWeight(1);
+            line(this.width/2, 0, this.width/2, this.height);
+            pop();
+
+            //Layout tick marks\\
+            let tickX = this.height / this.resolution;
+            let tickY = this.width / this.resolution;
+            let tickLen = 3;
+            
+            for (let i = 0; i <= this.resolution; i++){
+                let x = tickX*i;
+                let y = tickY*i;
+                stroke(0,0,0, 255);
+                push();
+                strokeWeight(1);
+                if (i != this.resolution/2){
+                    line(x, this.height/2-tickLen, x, this.height/2+tickLen);
+                    line(this.width/2-tickLen, y, this.width/2+tickLen, y);
+                }
+                
+                line()
+                pop();
+
+            }
+        }
+        
+    }
+
+}
---
+++ sketch.js
@@ -0,0 +1,171 @@
+
+var grid;
+var opacitySlider;
+var resSlider;
+var baseNumber = new p5.Vector(1, 1);
+var power = 10;
+calculate = true;
+let outputPoints1 = [];
+let outputPoints2 = [];
+let outputPoints3 = [];
+let outputPoints4 = [];
+let outputPoints5 = [];
+let outputPoints6 = [];
+let numbaz = [];
+var resIn = 20;
+
+
+var n;
+
+
+function h1(t) {
+    let f1 = 2 * pow(t, 3);
+
+    let f2 = 3 * pow(t, 2);
+
+    return f1 - f2 + 1;
+  }
+
+  function h2(t) {
+    let f1 = -2 * pow(t, 3);
+    let f2 = 3 * pow(t, 2);
+    return f1 + f2;
+  }
+
+function h3(t) {
+    let f1 = pow(t, 3);
+    let f2 = 2 * pow(t, 2);
+    return f1 - f2 + t;
+  }
+
+ function h4(t) {
+    let f1 = pow(t, 3);
+    let f2 = pow(t, 2);
+    return f1 - f2;
+  }
+
+// function mouseIsPressed(){
+//     grid.setOrigin(mouseX, mouseY);
+// }
+
+function mouseDragged(){
+    grid.setOrigin(mouseX, mouseY);
+
+}
+
+
+function setup(){
+    createCanvas(700, 700);
+
+    grid = new GridDraw(10, 0, 500, 500, 350, 350);
+    grid.setLineWidth(1);
+    opacitySlider = createSlider(0, 100, 10, 0.10);
+    opacitySlider.position(120,130);
+
+    resSlider = createSlider(0, 100, 10, 1);
+    resSlider.position(120,150);
+    grid.setText(false, 6);
+    n = createVector(baseNumber.x, baseNumber.y);
+}
+
+// function mousePressed(){
+//     console.log('basenumber was: ' + baseNumber)
+//     console.log('n was: ' + n)
+//     baseNumber.mult(n);
+//     outputPoints.push(createVector(baseNumber.x, baseNumber.y));
+//     console.log('basenumber is now: ' + baseNumber)
+//     console.log('n is now: ' + n);
+
+// }    
+
+function draw(){
+
+
+    background(100);
+    let opacVal = opacitySlider.value();
+    let resVal = resSlider.value();
+    
+
+    grid.setResolution(resIn);
+    grid.setGridOpacity(opacVal);
+
+    grid.show();
+    let inc = 1/ power;
+    let index = 0;
+    if (calculate){
+        for (let i = 0; i <= power; i++){
+            // let num1 =2*pow(index, 3);
+            // let num2 =3*pow(index, 2);
+            // let num3 = num1 - num2 + 1;
+            // let num4 = 6*pow(index,2);
+            // let num5 = 6*index;
+            // let num6 = num4 - num5;
+
+            let num1 = pow(index, 3);
+            let num2 = pow(index, 2);
+            let num3 = num1 - num2 + 1;
+            let num4 = 3 * pow(index,2);
+            let num5 = 2*index;
+            let num6 = num4 - num5;
+
+            
+            
+            outputPoints1.push(createVector(index*10, num1*10));
+            outputPoints2.push(createVector(index*10, num2*10));
+            outputPoints3.push(createVector(index*10, num3));
+            // outputPoints4.push(createVector(index*10, num4*10));
+            // outputPoints5.push(createVector(index*10, num5*10));
+            outputPoints6.push(createVector(index*10, num6));
+
+            
+
+
+
+            // let numX = num1*baseNumber.x;
+            // let numY = num1*baseNumber.y;
+
+
+            // text(n, n.x, n.y);
+            // if (i == power - 2){
+            //     calculate = false;
+            // }
+            index += inc;
+        }
+    }
+
+    // for (let points of outputPoints1){
+    //     grid.setPoint(points.x, points.y, 255,0,0);
+    // }
+
+    // for (let points of outputPoints2){
+    //     grid.setPoint(points.x, points.y, 0,255,0);
+    // }
+
+    for (let i = 0; i < outputPoints3.length; i++){
+        grid.setPoint(outputPoints3[i].x, outputPoints3[i].y*10, 0,0,255);
+        // outputPoints6[i].normalize();
+        grid.setPoint(outputPoints3[i].x, outputPoints6[i].y, 255,0,255);
+        numbaz.push([round(outputPoints3[i].y,5), round(outputPoints6[i].y,5)]);
+
+    }
+    
+    // for (let points of outputPoints6){
+    //     grid.setPoint(points.x, points.y, 255,0,255);
+    // }
+    // if (frameCount == 1){
+    //     console.table(numbaz);
+
+    // }
+    
+
+}
+
+window.addEventListener("wheel", function(e) {
+    if (e.deltaY > 0){
+        resIn *= 1.05;}
+    else {
+        resIn *= .95;
+    }
+
+
+  });
